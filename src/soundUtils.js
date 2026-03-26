746707dcee8f164e6670b041f8b0c9ab782b65ce
carlitoooooooooo <carlito@copdrop.app>
2026-03-25 22:01:20 -0400

    Set default sound volume to 25%

--- src/soundUtils.js
+++ src/soundUtils.js
@@ -60,9 +60,9 @@ export function setNotificationSoundType(type) {
  */
 export function getNotificationVolume() {
   try {
-    return parseInt(localStorage.getItem(SOUND_VOLUME_KEY) || '100', 10);
+    return parseInt(localStorage.getItem(SOUND_VOLUME_KEY) || '25', 10);
   } catch {
-    return 100;
+    return 25;
   }
 }
 
